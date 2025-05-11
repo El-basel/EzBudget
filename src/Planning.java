228486204cae1485f5084c8cac994a153c924ca2
xelafares <xelafares2006@gmail.com>
2025-05-11 15:20:29 +0300

    Removed userID instances.

--- src/Planning.java
+++ src/Planning.java
@@ -27,37 +27,18 @@ public class Planning {
         database.insertGoal(goal);
     }
 
-    public void createSavingPlan(int goal, int startAmount, int monthlyContribution){
-        SavingPlan savingPlan = new SavingPlan(goal, startAmount, monthlyContribution);
-        //database.insertSavingPlan(savingPlan);
-    }
-
-    //add user id in constructor
-    public void setReminder(String title, String date, String message){
-        Reminder reminder = new Reminder();
-        reminder.setTitle(title);
-        reminder.setDate(date);
-        reminder.setMessage(message);
-        //database.insertReminder(reminder);
-    }
-
     //change to return Goal when goal is implemented
-    public void getGoal(int goalID){
-        //return database.retrieveGoal(goalID);
-    }
+//    public Goal getGoal(int goalID){
+//        return database.retrieveGoal(goalID);
+//    }
 
     //change to return Goal[]
-    public void getGoals(){
-        //return database.retrieveGoals();
-    }
+//    public void getGoals(){
+//        //return database.retrieveGoals();
+//    }
 
-    //change to return SavingPlan when it is implemented
-    public void getplan(int planID){
-        //return database.retrieveSavingPlan(planID);
-    }
-
-    //change to return SavingPlan[] when it is implemented
-    public void getplans(){
-        //return database.retrieveSavingPlans();
-    }
+//    public boolean setReminder(String title, String date, String message){
+//        Reminder reminder = new Reminder(title, date, message);
+//        return database.insertReminder(reminder);
+//    }
 }
